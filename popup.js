ad11e2da2997feec07c3a70fdcd40d29e95ce7ac
Tereza <tereza.zizkova366@gmail.com>
2025-02-23 14:02:02 +0100

    vycisteni obsah od databaze - vse funguje na st atickem nacitani obsahu z storyData v storyOfTheDay.js

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 import { createNasaSection } from "./scripts/components/nasa.js";
 import { createHeckMindset } from "./scripts/components/heckMindset.js";
 import { createStoryOfTheDay } from "./scripts/components/storyOfTheDay.js";
-// import { fetchStory } from "./scripts/database.js"; // ✅ Import databázové funkce
 
 console.log(("✅ Popup.js načten správně!"))
 
@@ -14,14 +13,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const heckMindset = await createHeckMindset()
     const nasaSection = await createNasaSection()
     let storyOfTheDay = await createStoryOfTheDay()
-
-    // 📡 Načtení příběhu z databáze
-    // const storyData = await fetchStory();
-    
-    // 🔄 Aktualizace obsahu příběhu
-    // storyOfTheDay.querySelector("h2").textContent = storyData.title;
-    // storyOfTheDay.querySelector("p").textContent = storyData.content;
-    // storyOfTheDay.querySelector("cite").textContent = storyData.author;
 
     body.appendChild(heckMindset)
     body.appendChild(nasaSection)
