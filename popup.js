41fd8c39c77b707c2a1e83def8971dadf4405f4c
Tereza <tereza.zizkova366@gmail.com>
2025-02-23 14:06:44 +0100

    vycisteni obsahu - nacita se pouze heckMindset, img NASA, popis, odkaz na stranku

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 import { createNasaSection } from "./scripts/components/nasa.js";
 import { createHeckMindset } from "./scripts/components/heckMindset.js";
-// import { createStoryOfTheDay } from "./scripts/components/storyOfTheDay.js";
-// import { fetchStory } from "./scripts/database.js"; // ✅ Import databázové funkce
 
 console.log(("✅ Popup.js načten správně!"))
 
@@ -13,19 +11,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const heckMindset = await createHeckMindset()
     const nasaSection = await createNasaSection()
-    // let storyOfTheDay = await createStoryOfTheDay()
-
-    // 📡 Načtení příběhu z databáze
-    // const storyData = await fetchStory();
-    
-    // 🔄 Aktualizace obsahu příběhu
-    // storyOfTheDay.querySelector("h2").textContent = storyData.title;
-    // storyOfTheDay.querySelector("p").textContent = storyData.content;
-    // storyOfTheDay.querySelector("cite").textContent = storyData.author;
 
     body.appendChild(heckMindset)
     body.appendChild(nasaSection)
-    // body.appendChild(storyOfTheDay)
 
     console.log("✅ Všechny sekce byly přidány!");
 })
