cfc09959003467e92295fc5b83adef38bfcda3e0
Tereza <tereza.zizkova366@gmail.com>
2025-04-09 16:20:08 +0200

    prepsani nazvu z fetch.js > fetchNasa.js = pridany fetchStoryOfTheDay.js - pro lepsi prehlednost zmena nazvu. fetchStory....js jsem pridala kod pro stahovani z backend z db. - zatim nefunguje, dodelam zitra, ale fungovat bude ✌️

---
+++ frontend/scripts/fetch/fetchStoryOfTheDay.js
@@ -0,0 +1,14 @@
+console.log("📖 fetchStoryOfTheDay je načtený");
+
+export async function fetchStoryOfTheDay() {
+    try {
+        const response = await fetch("api/story-of-the-day")
+        if (!response.ok) throw new Error("Chyba pri nacitani dat")
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error("fetchStoryOfTheDay error", error);
+        return null
+    }
+}
