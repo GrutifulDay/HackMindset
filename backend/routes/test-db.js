5f184529af19df2f0a9a42e41267388fdb86b919
Tereza <tereza.zizkova366@gmail.com>
2025-04-28 14:14:05 +0200

    odkomentovani

--- backend/routes/test-db.js
+++ backend/routes/test-db.js
@@ -4,21 +4,21 @@ import { addToBlacklist } from "../middlewares/ipBlacklist.js";
 
 const router = express.Router();
 
-// const getCityByIP = async (ip) => {
-//   const realIP =
-//     ip === "::1" || ip === "::ffff:127.0.0.1" || ip === "127.0.0.1"
-//       ? "8.8.8.8" // Google DNS – veřejná IP pro test
-//       : ip;
+const getCityByIP = async (ip) => {
+  const realIP =
+    ip === "::1" || ip === "::ffff:127.0.0.1" || ip === "127.0.0.1"
+      ? "8.8.8.8" // Google DNS – veřejná IP pro test
+      : ip;
 
-//   try {
-//     const response = await fetch(`https://ipapi.co/${realIP}/json/`);
-//     const data = await response.json();
-//     return data.city || "Neznámé město";
-//   } catch (err) {
-//     console.error("❌ Chyba při získávání města:", err.message);
-//     return "Neznámé město";
-//   }
-// };
+  try {
+    const response = await fetch(`https://ipapi.co/${realIP}/json/`);
+    const data = await response.json();
+    return data.city || "Neznámé město";
+  } catch (err) {
+    console.error("❌ Chyba při získávání města:", err.message);
+    return "Neznámé město";
+  }
+};
 
 
 router.get("/test-db", async (req, res) => {
@@ -27,20 +27,20 @@ router.get("/test-db", async (req, res) => {
   const parser = new UAParser(userAgentString);
   const result = parser.getResult();
 
-  // const city = await getCityByIP(userIP);
+  const city = await getCityByIP(userIP);
 
   await addToBlacklist(userIP, "Test logování s městem", {
     userAgent: userAgentString,
     browser: result.browser.name,
     os: result.os.name,
     deviceType: result.device.type,
-    // city: city
+    city: city
   });
 
   res.json({
     message: "Uloženo do DB i s městem",
     ip: userIP,
-    // city: city,
+    city: city,
     parsed: result
   });
 });
