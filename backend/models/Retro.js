918d5d3ac728b86f97374922ba8e2a6596b9f9e7
Tereza <tereza.zizkova366@gmail.com>
2025-05-26 09:24:16 +0200

    date presunuty

--- backend/models/Retro.js
+++ backend/models/Retro.js
@@ -4,12 +4,12 @@ import connectFrontendDB from "../db/connectFrontendDB.js";
 const frontendConnection = connectFrontendDB();
 
 const retroSchema = new mongoose.Schema({
+    date: String,
     years: String,
     title: String,
     nostalgiggle: String,
     like: String,
     dislike: String,
-    date: String 
 })
 
 export default frontendConnection.model("retro", retroSchema)
