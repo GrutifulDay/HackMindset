a367003fad21eabd6681ae1568f4d009fcd2941f
Tereza <tereza.zizkova366@gmail.com>
2025-06-17 14:53:37 +0200

    presun do app slozky

---
+++ frontend/app/utils/config.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://localhost:3000/api"
+
+export const API = {
+  nasa: `${BASE_URL}/nasa`,
+  profile: `${BASE_URL}/profile`,
+  retroMachine: `${BASE_URL}/retro-machine`,
+  storyOfTheDay: `${BASE_URL}/story-of-the-day`,
+}
+
+export const DEV_MODE = false // true / false 
+
+// chrome.storage.local.clear() do konzole pro vymaz 
