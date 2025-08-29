806b71437a52450b17b38649d5eefeced85992f1
Tereza <tereza.zizkova366@gmail.com>
2025-08-29 11:20:30 +0200

    zmena base url

--- frontend/app/utils/config.js
+++ frontend/app/utils/config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://hackmindset.app/api"
-const BASE_URL = "https://localhost:3000/api"
+const BASE_URL = "https://hackmindset.app/api"
+// const BASE_URL = "https://localhost:3000/api"
 
 export const API = {
   nasa: `${BASE_URL}/nasa`,
