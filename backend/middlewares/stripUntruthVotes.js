55628dbe12a078f588e9f08cf09735a152e7bd75
Tereza <tereza.zizkova366@gmail.com>
2025-08-31 16:35:33 +0200

    predesly kod nefungoval - zmena

--- backend/middlewares/stripUntruthVotes.js
+++ backend/middlewares/stripUntruthVotes.js
@@ -1,24 +1,39 @@
-export default function stripUntruthVotes(_req, res, next) {
-    const origJson = res.json.bind(res);
+// backend/middlewares/stripUntruthVotes.js
+function scrub(obj) {
+    if (!obj || typeof obj !== "object") return;
+    if ("untruthVotes" in obj) delete obj.untruthVotes;
+  }
   
+  export default function stripUntruthVotes(_req, res, next) {
+    const origJson = res.json.bind(res);
+    const origSend = res.send.bind(res);
+  
+    // zachyti v json
     res.json = (body) => {
       try {
-        // Odeber z objektu
-        const scrub = (obj) => {
-          if (obj && typeof obj === "object") {
-            if ("untruthVotes" in obj) delete obj.untruthVotes;
-          }
-        };
-  
-        if (Array.isArray(body)) {
-          body.forEach(scrub);
-        } else {
-          scrub(body);
-        }
-      } catch (_) {
-        // nic – nechceme rozbít odpověď kvůli chybě při mazání
-      }
+        if (Array.isArray(body)) body.forEach(scrub);
+        else scrub(body);
+      } catch (_) {}
       return origJson(body);
+    };
+  
+    // zachyti i .send() (string nebo object)
+    res.send = (body) => {
+      try {
+        if (typeof body === "string") {
+          try {
+            const parsed = JSON.parse(body);
+            if (Array.isArray(parsed)) parsed.forEach(scrub);
+            else scrub(parsed);
+            res.set("Content-Type", "application/json; charset=utf-8");
+            return origSend(JSON.stringify(parsed));
+          } catch { /* neni to json string → posle dal bezezmeny */ }
+        } else if (body && typeof body === "object") {
+          if (Array.isArray(body)) body.forEach(scrub);
+          else scrub(body);
+        }
+      } catch (_) {}
+      return origSend(body);
     };
   
     next();
