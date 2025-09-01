87866838504d67b9bc37bf2282d9c56d0f0e275d
Tereza <tereza.zizkova366@gmail.com>
2025-09-01 15:12:35 +0200

    nacetlo se hlasovani > po aktualyzaci se vynulovalo > ch yba v pristupu - spatny cors + falied fetch

--- frontend/app/fetch/fetchStoryVotes.js
+++ frontend/app/fetch/fetchStoryVotes.js
@@ -7,7 +7,7 @@ export async function fetchGetVoteStory(date) {
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer HACK_EXTENSION"
+        "X-Client-Tag": "HACK_EXTENSION"
       }
     })
     return await response.json()
