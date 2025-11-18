e13178a9bffbd4c534ade08d60872d7c3c55da1f
Tereza <tereza.zizkova366@gmail.com>
2025-11-18 13:44:14 +0100

    false na loopback

--- backend/server.js
+++ backend/server.js
@@ -53,8 +53,8 @@ import connectFrontendDB from "./db/connectFrontendDB.js"
 import path from "path"
 
 const app = express()
-//app.set("trust proxy", "loopback"); 
-app.set("trust proxy", false); // true = proxy / false = vyvoj 
+app.set("trust proxy", "loopback"); 
+//app.set("trust proxy", false); // true = proxy / false = vyvoj 
 
 app.disable("etag")
 app.disable("x-powered-by")
