9e8c93c053cda787c45b795787ebf76ffc1ea965
Tereza <tereza.zizkova366@gmail.com>
2025-11-20 11:38:16 +0100

    zkouska pridani helmet moje url

--- backend/server.js
+++ backend/server.js
@@ -111,7 +111,7 @@ app.use(
         "https://mars.nasa.gov",
         "https://images-assets.nasa.gov"
       ],
-      "connect-src": ["'self'", "https://api.nasa.gov"],
+      "connect-src": ["'self'", "https://api.nasa.gov", "https://hackmindset.app"],
       "base-uri": ["'self'"],
       "object-src": ["'none'"],
       "frame-ancestors": ["'none'"]
